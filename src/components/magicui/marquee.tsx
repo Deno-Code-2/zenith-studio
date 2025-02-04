665be21c65c42be5a920f882f22e96ef9359ac2a
Deno-Code-2 <syedmoinuddin597@gmail.com>
2025-02-05 01:10:37 +0530

    Update marquee.tsx

--- src/components/magicui/marquee.tsx
+++ src/components/magicui/marquee.tsx
@@ -1,54 +1,51 @@
-import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface MarqueeProps extends React.HTMLAttributes<HTMLDivElement> {
-  pauseOnHover?: boolean;
+interface MarqueeProps {
+  className?: string;
   reverse?: boolean;
-  fade?: boolean;
+  pauseOnHover?: boolean;
+  children?: React.ReactNode;
+  vertical?: boolean;
+  repeat?: number;
+  [key: string]: any;
 }
 
-export function Marquee({
-  children,
+export default function Marquee({
   className,
   reverse,
-  pauseOnHover,
-  fade,
+  pauseOnHover = false,
+  children,
+  vertical = false,
+  repeat = 4,
   ...props
 }: MarqueeProps) {
   return (
     <div
-      className={cn(
-        "group flex overflow-hidden [--duration:40s] [--gap:1rem]",
-        className
-      )}
       {...props}
-    >
-      <div
-        className={cn(
-          "flex min-w-full shrink-0 items-center justify-around gap-[--gap] [--play-state:running] [animation:scroll_var(--duration)_linear_infinite]",
-          reverse && "[animation-direction:reverse]",
-          pauseOnHover && "group-hover:[animation-play-state:paused]"
-        )}
-      >
-        {children}
-      </div>
-      <div
-        className={cn(
-          "flex min-w-full shrink-0 items-center justify-around gap-[--gap] [--play-state:running] [animation:scroll_var(--duration)_linear_infinite]",
-          reverse && "[animation-direction:reverse]",
-          pauseOnHover && "group-hover:[animation-play-state:paused]"
-        )}
-        aria-hidden
-      >
-        {children}
-      </div>
-
-      {fade && (
-        <>
-          <div className="pointer-events-none absolute left-0 top-0 h-full w-1/4 bg-gradient-to-r from-background to-transparent"></div>
-          <div className="pointer-events-none absolute right-0 top-0 h-full w-1/4 bg-gradient-to-l from-background to-transparent"></div>
-        </>
+      className={cn(
+        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+        {
+          "flex-row": !vertical,
+          "flex-col": vertical,
+        },
+        className,
       )}
+    >
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
+              "animate-marquee flex-row": !vertical,
+              "animate-marquee-vertical flex-col": vertical,
+              "group-hover:[animation-play-state:paused]": pauseOnHover,
+              "[animation-direction:reverse]": reverse,
+            })}
+          >
+            {children}
+          </div>
+        ))}
     </div>
   );
 }
