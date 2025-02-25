83c1993e58cbaad912c53079f7b33edb3abd5956
Sam <syedmoinuddin597@gmail.com>
2025-02-25 13:32:16 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -96,10 +96,10 @@ const Header = () => {
               <Button 
                 variant="outline"
                 onClick={() => {
-                  window.open("https://calendly.com/demo/30min", "_blank");
+                  window.open("https://calendly.com/syedmoinuddin106", "_blank");
                   setIsMenuOpen(false);
                 }}
-                className="border-custom-orange/20 text-white hover:bg-custom-orange/10"
+                className="border-custom-orange/20 text-white font-jakarta hover:bg-custom-orange/10"
               >
                 <Calendar className="w-5 h-5 mr-2" />
                 Book a call
