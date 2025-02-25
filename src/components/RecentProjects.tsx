bf766a5756d74188db9fe97c5c01695bfe9ad8b7
Sam <syedmoinuddin597@gmail.com>
2025-02-25 17:54:56 +0530

    Update RecentProjects.tsx

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -53,15 +53,17 @@ const RecentProjects = () => {
 
   return (
     <section className="py-20 bg-black">
-      <div className="container mx-auto px-8 lg:px-16">
-        <div className="text-center mb-16">
-          <h2 className="text-4xl font-bold mb-4 font-syne">
-            Tech <span className="text-custom-orange">MEETS</span>Precision
-          </h2>
-          <p className="text-center text-gray-300 font-jakarta max-w-2xl">
-            Zenith Studio's recent projects feature cutting-edge design, user-friendly interfaces, and tailored solutions for clients.
-          </p>
-        </div>
+      <div className="flex flex-col items-center justify-center min-h-screen container mx-auto px-8 lg:px-16">
+  <div className="text-center mb-16">
+    <h2 className="text-4xl font-bold mb-4 font-syne">
+      Tech <span className="text-custom-orange">MEETS</span> Precision
+    </h2>
+    <p className="text-center text-gray-300 font-jakarta max-w-2xl">
+      Zenith Studio's recent projects feature cutting-edge design, user-friendly interfaces, and tailored solutions for clients.
+    </p>
+  </div>
+</div>
+
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {projects?.map(project => (
