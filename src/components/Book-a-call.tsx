b4f15fd207aac981608e24b2285d74b6b523b883
SyedMoin-Lab <syedmoinuddin106@gmail.com>
2025-04-17 20:47:59 +0530

    updated fixed

--- src/components/Book-a-call.tsx
+++ src/components/Book-a-call.tsx
@@ -14,10 +14,23 @@ export default function Bookacall() {
   }, []);
 
   return (
-    <Cal
-      namespace="30min"
-      calLink="zenithstudio/30min"
-      style={{ width: "100%", height: "100%" }}
-    />
+    <div style={{ 
+      width: "100%", 
+      minHeight: "600px", 
+      margin: "20px 0",
+      padding: "20px",
+      backgroundColor: "#f5f5f5",
+      borderRadius: "8px"
+    }}>
+      <Cal
+        namespace="30min"
+        calLink="zenithstudio/30min"
+        style={{ 
+          width: "100%", 
+          height: "100%",
+          minHeight: "600px"
+        }}
+      />
+    </div>
   );
 }
