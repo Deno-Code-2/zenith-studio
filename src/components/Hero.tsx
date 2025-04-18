abd1db41a5fa086591ce4683f215d0678a1b15ec
SyedMoin-Lab <syedmoinuddin106@gmail.com>
2025-04-18 21:01:34 +0530

    new hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -62,7 +62,7 @@ const Hero = () => {
       </div>
 
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24 lg:py-32">
-        <div className="flex flex-col items-center justify-center text-center mt-[-40px]">
+        <div className="flex flex-col items-center justify-center text-center mt-[-83px]">
           <motion.div
             variants={containerVariants}
             initial="hidden"
